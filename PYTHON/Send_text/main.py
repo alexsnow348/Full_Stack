affea0f87d0ca522f5981e72a74d1382d1fa625b
Alex Snow <42266513+alexsnow348@users.noreply.github.com>
2018-11-12 15:24:45 +0800

    Update main.py

--- PYTHON/Send_text/main.py
+++ PYTHON/Send_text/main.py
@@ -1,9 +1,9 @@
 from twilio.rest import Client
 
 # Your Account SID from twilio.com/console
-account_sid = "AC0dadb0ce3f1db887ecf2cc5209932676"
+account_sid = ""
 # Your Auth Token from twilio.com/console
-auth_token  = "7eb9f55beeb3cfea278ccc183cf0f46a"
+auth_token  = ""
 
 client = Client(account_sid, auth_token)
 
